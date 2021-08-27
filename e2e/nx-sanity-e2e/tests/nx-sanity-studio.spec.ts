b0b53d8c31aba0a508a1c1cf539085cad905f35d
Jonathan A. Mitchell <ontoneio.mm@gmail.com>
2021-08-26 18:01:38 -0700

    WIP

--- e2e/nx-sanity-e2e/tests/nx-sanity-studio.spec.ts
+++ e2e/nx-sanity-e2e/tests/nx-sanity-studio.spec.ts
@@ -1,66 +1,84 @@
-import {
-    checkFilesExist,
-    ensureNxProject,
-    readJson,
-    runNxCommandAsync,
-    uniq,
-  } from '@nrwl/nx-plugin/testing';
+// import {
+//     checkFilesExist,
+//     ensureNxProject,
+//     readJson,
+//     runNxCommandAsync,
+//     uniq,
+//   } from '@nrwl/nx-plugin/testing';
 
-import { readWorkspaceConfiguration, Tree } from '@nrwl/devkit';
-import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
-// import reactInitGenerator from './init';
-// import { InitSchema } from './schema';
+// import { readWorkspaceConfiguration, Tree } from '@nrwl/devkit';
+// import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
+// // import reactInitGenerator from './init';
+// // import { InitSchema } from './schema';
 
 
   
-  describe('Initialize Sanity Project', () => {
+// describe('Initialize Sanity Studio Application Project', () => {
 
-    beforeEach(async () => {
-      const plugin = uniq('nx-sanity');
-      ensureNxProject('@otio/nx-sanity', 'dist/packages/nx-sanity');
-      await runNxCommandAsync(`generate @otio/nx-sanity:nx-sanity ${plugin}`);
-    })
+//   let appTree: Tree;
+//   let schema: Schema = {
+//     babelJest: false,
+//     e2eTestRunner: 'cypress',
+//     skipFormat: false,
+//     unitTestRunner: 'jest',
+//     name: 'myApp',
+//     linter: Linter.EsLint,
+//     style: 'css',
+//     strict: false,
+//     standaloneConfig: false,
+//   };
 
-    it('should create a sanity studio app', async () => { 
-      const result = await runNxCommandAsync(`build ${plugin}`);
-      expect(result.stdout).toContain('Executor ran');
-    }, 120000);
-  
-  });
+//   beforeEach(() => {
+//     appTree = createTreeWithEmptyWorkspace();
+//   });
 
-describe('init', () => {
-  let tree: Tree;
-  let schema: InitSchema = {
-    // unitTestRunner: 'jest',
-    // e2eTestRunner: 'cypress',
-    // skipFormat: false,
-  };
+//   // beforeEach(async () => {
+//   //   const plugin = uniq('nx-sanity');
+//   //   ensureNxProject('@otio/nx-sanity', 'dist/packages/nx-sanity');
+//   //   await runNxCommandAsync(`generate @otio/nx-sanity:nx-sanity ${plugin}`);
+//   // })
 
-  beforeEach(() => {
-    tree = createTreeWithEmptyWorkspace();
-  });
+//   // it('should create a sanity studio app', async () => { 
+//   //   const result = await runNxCommandAsync(`build ${plugin}`);
+//   //   expect(result.stdout).toContain('Executor ran');
+//   // }, 120000);
 
-  it('should add @sanity/cli dependencies', async () => {
-    await reactInitGenerator(tree, schema);
-    const packageJson = readJson(tree, 'package.json');
-    expect(packageJson.dependencies['react']).toBeDefined();
-    expect(packageJson.dependencies['react-dom']).toBeDefined();
-    expect(packageJson.devDependencies['@types/react']).toBeDefined();
-    expect(packageJson.devDependencies['@types/react-dom']).toBeDefined();
-    expect(packageJson.devDependencies['@testing-library/react']).toBeDefined();
-  });
+// });
 
-  describe('defaultCollection', () => {
-    it('should be set if none was set before', async () => {
-      await reactInitGenerator(tree, schema);
-      const workspace = readWorkspaceConfiguration(tree);
-      expect(workspace.cli.defaultCollection).toEqual('@nrwl/react');
-      expect(workspace.generators['@nrwl/react'].application.babel).toBe(true);
-    });
-  });
+// describe('init', () => {
+//   let tree: Tree;
+//   let schema: InitSchema = {
+//     // unitTestRunner: 'jest',
+//     // e2eTestRunner: 'cypress',
+//     // skipFormat: false,
+//   };
 
-  it('should not add jest config if unitTestRunner is none', async () => {
-    await reactInitGenerator(tree, { ...schema, unitTestRunner: 'none' });
-    expect(tree.exists('jest.config.js')).toEqual(false);
-  });
-});
+//   beforeEach(() => {
+//     tree = createTreeWithEmptyWorkspace();
+//   });
+
+//   it('should add @sanity/cli dependencies', async () => {
+//     await reactInitGenerator(tree, schema);
+//     const packageJson = readJson(tree, 'package.json');
+//     expect(packageJson.dependencies['react']).toBeDefined();
+//     expect(packageJson.dependencies['react-dom']).toBeDefined();
+//     expect(packageJson.devDependencies['@types/react']).toBeDefined();
+//     expect(packageJson.devDependencies['@types/react-dom']).toBeDefined();
+//     expect(packageJson.devDependencies['@testing-library/react']).toBeDefined();
+//   });
+
+//   // describe('defaultCollection', () => {
+//   //   it('should be set if none was set before', async () => {
+//   //     await reactInitGenerator(tree, schema);
+//   //     const workspace = readWorkspaceConfiguration(tree);
+//   //     expect(workspace.cli.defaultCollection).toEqual('@nrwl/react');
+//   //     expect(workspace.generators['@nrwl/react'].application.babel).toBe(true);
+//   //   });
+//   // });
+
+//   // it('should not add jest config if unitTestRunner is none', async () => {
+//   //   await reactInitGenerator(tree, { ...schema, unitTestRunner: 'none' });
+//   //   expect(tree.exists('jest.config.js')).toEqual(false);
+//   // });
+
+// });
